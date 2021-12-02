58b78e6fedbc157697f9275a916562e54f1b1b2c
uniqemejiya <95008229+uniqemejiya@users.noreply.github.com>
2021-12-02 18:14:54 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -8,11 +8,6 @@ var canvas;
 var palyer, playerBase;
 var computer, computerBase;
 
-//Declare an array for arrows playerArrows = [ ]
-var playerArrows = [];
-
-var arrow;
-
 
 function setup() {
   canvas = createCanvas(windowWidth, windowHeight);
@@ -21,13 +16,11 @@ function setup() {
   world = engine.world;
 
   playerBase = new PlayerBase(300, random(450, height - 300), 180, 150);
-  player = new Player(285, playerBase.body.position.y - 153, 50, 180);
-  playerArcher = new PlayerArcher(
-    340,
-    playerBase.body.position.y - 180,
-    120,
-    120
-  );
+  
+  //player = new ( 340, playerBase.position.y - 112, 120, 120);
+ player = new Player(285, playerBase.body.position.y - 153, 50, 180);
+ // player =  Player( 340, playerBase.position.y - 112, 120, 120);
+ // player= new Player( )
 
   computerBase = new ComputerBase(
     width - 300,
@@ -41,20 +34,12 @@ function setup() {
     50,
     180
   );
-  computerArcher = new ComputerArcher(
-    width - 340,
-    computerBase.body.position.y - 180,
-    120,
-    120
-  );
+
   
- 
-
-
 }
 
 function draw() {
-  background(180);
+  background(189);
 
   Engine.update(engine);
 
@@ -66,97 +51,14 @@ function draw() {
 
  
   playerBase.display();
+  
+  //display();
+  //playerdisplay();
+  //player();
   player.display();
   
 
   computerBase.display();
   computer.display();
   
-  playerArcher.display();
-  computerArcher.display()
-
- // Uncomment and use correct for loop to display arrow using showArrow() function
- for (var i=0; i<playerArrows.length; i++) 
-  {
-  showArrows(i, playerArrows);
-  }
-
- // for (var i=0; i<playerArrows; i++) 
- //{
-//showArrows(i, playerArrows);
-  //}
-
-  //for (var i=0; i<playerArrows.length; i++) 
-// {
-  //showArrows(playerArrows, i);
- // }
-
-
-
-
-
-}
-
-/*********** Choose correct keyPressed() function out of these *************/
-
- function keyPressed() {
-     // create an arrow object and add into an array ; set its angle same as angle of playerArcher
-     var posX = playerArcher.body.position.x;
-     var posY = playerArcher.body.position.y;
-     var angle = playerArcher.body.angle+PI/2;
-     var arrow = new PlayerArrow(posX, posY, 100, 10);
-     arrow.trajectory = [];
-     Matter.Body.setAngle(arrow.body, angle);
-     playerArrows.push(arrow);
- }
-
-
- //function keyPressed() {
-  // if(keyCode === 32){
-     // create an arrow object and add into an array ; set its angle same as angle of playerArcher
-    // var posX = playerArcher.body.position.x;
-//var posY = playerArcher.body.position.y;
-    // var angle = playerArcher.body.angle+PI/2;
-    // var arrow = new PlayerArrow(posX, posY, 100, 10);
-    //arrow.trajectory = [];
-    // Matter.Body.setAngle(arrow.body, angle);
-    // playerArrows.push(arrow);
-  // }
-// }
-
-
- //function keyPressed() {
-   //if(keyCode === 32){
-     // create an arrow object and add into an array ; set its angle same as angle of playerArcher
-     //var posX = playerArcher.body.position.x;
-     //var posY = playerArcher.body.position.y;
-     //var angle = playerArcher.body.angle+PI/2;
-     //Matter.Body.setAngle(arrow.body, angle);
-    // playerArrows.push(arrow);
- // }
-// }
-
-
-
-
-
-function keyReleased () {
-
-  if(keyCode === 32){
-    //call shoot() function for each arrow in an array playerArrows
-    if (playerArrows.length) {
-      var angle = playerArcher.body.angle+PI/2;
-      playerArrows[playerArrows.length - 1].shoot(angle);
-    }
-  }
-
-}
-//Display arrow and Tranjectory
-function showArrows(index, arrows) {
-  arrows[index].display();
-  
-    
-  
- 
-
 }
